d354c853a46468e5196260c485982176c55c3f2a
Shivam250202 <shivampandey5897@gmail.com>
2024-07-29 14:14:13 +0530

    My First Commit

---
+++ index.php
@@ -0,0 +1,126 @@
+<?php
+/*connect to the database
+*/
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "Crud";
+
+//create a connection
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// if connection was not successfull
+
+if (!$conn) {
+    die("sorry we failed to connect:" . mysqli_connect_error());
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $Title = $_POST["Title"];
+    $Description = $_POST["Description"];
+
+    // sql query to be executed
+    $sql = "INSERT INTO `crud` (`Title`, `Description`, ) VALUES ('$Title', '$Description')";
+    $result = mysqli_query($conn, $sql);
+
+    //Add new trip to the Trip table in the database
+    if ($result) {
+        echo "The record has been Inserted successfull<br>";
+    } else {
+        echo "The record was not inserted successfully because of this error ----> " . mysqli_error($conn);
+    }
+}
+
+/*else{
+    echo "Connection was successfull<br>";
+} */
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRUD PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">PHP CRUD</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <div class="container my-5">
+        <h2>Add a Note</h2>
+        <form action="/CRUD PHP/index.php" method="post">
+            <div class="mb-3">
+                <label for="Title" class="form-label">Node Title</label>
+                <input type="text" class="form-control" id="Title" name="Title">
+
+            </div>
+            <div class="mb-3">
+                <label for="desc" class="form-label">Node Discription</label>
+                <textarea class="form-control" id="Description" name="Description" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">ADD Note</button>
+        </form>
+    </div>
+    <div class="container">
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">S.No</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM `crud`";
+                $result = mysqli_query($conn, $sql);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>
+                    <th scope='row'>" . $row['S.No.'] . "</th>
+                    <td>" . $row['Title'] . "</td>
+                    <td>" . $row['Description'] . "</td>
+                    <td> Actions </td>
+                </tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+
+</html>
