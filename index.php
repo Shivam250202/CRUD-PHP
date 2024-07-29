77831beb786c9a2cd11ad4bce033d840025638ba
Shivam250202 <shivampandey5897@gmail.com>
2024-07-29 19:15:06 +0530

    My Second Commit

--- index.php
+++ index.php
@@ -1,41 +1,35 @@
 <?php
-/*connect to the database
-*/
+/* Connect to the database */
+$insert = false;
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "Crud";
 
-//create a connection
-
+// Create a connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-// if connection was not successfull
-
+// If connection was not successful
 if (!$conn) {
-    die("sorry we failed to connect:" . mysqli_connect_error());
+    die("Sorry, we failed to connect: " . mysqli_connect_error());
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $Title = $_POST["Title"];
-    $Description = $_POST["Description"];
+    $Title = mysqli_real_escape_string($conn, $_POST["Title"]);
+    $Description = mysqli_real_escape_string($conn, $_POST["Description"]);
 
-    // sql query to be executed
-    $sql = "INSERT INTO `crud` (`Title`, `Description`, ) VALUES ('$Title', '$Description')";
+    // SQL query to be executed
+    $sql = "INSERT INTO `crud` (`Title`, `Description`) VALUES ('$Title', '$Description')";
     $result = mysqli_query($conn, $sql);
 
-    //Add new trip to the Trip table in the database
+    // Add new trip to the Trip table in the database
     if ($result) {
-        echo "The record has been Inserted successfull<br>";
+        // echo "The record has been Inserted successfully<br>";
+        $insert = true;
     } else {
         echo "The record was not inserted successfully because of this error ----> " . mysqli_error($conn);
     }
 }
-
-/*else{
-    echo "Connection was successfull<br>";
-} */
-
 ?>
 
 <!doctype html>
@@ -63,11 +57,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <li class="nav-item">
                         <a class="nav-link" href="#">About</a>
                     </li>
-                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Contact Us</a>
                     </li>
-
                 </ul>
                 <form class="d-flex" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -76,24 +68,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             </div>
         </div>
     </nav>
+
+    <?php
+    if ($insert) {
+        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+  <strong>Success!</strong> Your note has been inserted successfully.
+  <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+</div>";
+    }
+    ?>
+
     <div class="container my-5">
         <h2>Add a Note</h2>
         <form action="/CRUD PHP/index.php" method="post">
             <div class="mb-3">
-                <label for="Title" class="form-label">Node Title</label>
+                <label for="Title" class="form-label">Note Title</label>
                 <input type="text" class="form-control" id="Title" name="Title">
-
             </div>
             <div class="mb-3">
-                <label for="desc" class="form-label">Node Discription</label>
+                <label for="Description" class="form-label">Note Description</label>
                 <textarea class="form-control" id="Description" name="Description" rows="3"></textarea>
             </div>
-            <button type="submit" class="btn btn-primary">ADD Note</button>
+            <button type="submit" class="btn btn-primary">Add Note</button>
         </form>
     </div>
     <div class="container">
-
-        <table class="table">
+        <table class="table" id="myTable">
             <thead>
                 <tr>
                     <th scope="col">S.No</th>
@@ -121,6 +121,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+
+    <script>
+        let table = new DataTable('#myTable');
+    </script>
+
 </body>
 
 </html>
